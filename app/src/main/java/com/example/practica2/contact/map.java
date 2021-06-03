fa252fd6ae953525cb816141c7ecafce2525802c
JavsJavs <45043131+JavsJavs@users.noreply.github.com>
2021-06-03 02:16:02 +0200

    Map api implemented

--- app/src/main/java/com/example/practica2/contact/map.java
+++ app/src/main/java/com/example/practica2/contact/map.java
@@ -2,22 +2,33 @@ package com.example.practica2.contact;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import com.example.practica2.MainActivity;
 import com.example.practica2.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class map extends AppCompatActivity {
+public class map extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         TextView textLogo = findViewById(R.id.cubeBusterLogoMap);
         textLogo.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +38,49 @@ public class map extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng cubebustersHq = new LatLng(40.538430, -3.893596);
+        mMap.addMarker(new MarkerOptions().position(cubebustersHq).title("Cubebusters Headquarters"));
+        LatLng cubebustersMobile = new LatLng(20, -40);
+        mMap.addMarker(new MarkerOptions().position(cubebustersMobile).title("Cubebusters Mobile"));
+        LatLng cubebustersAsia = new LatLng(69, 69);
+        mMap.addMarker(new MarkerOptions().position(cubebustersAsia).title("Cubebusters Asia"));
+
+
+        LatLng cubebustersREye = new LatLng(-75, 72);
+        mMap.addMarker(new MarkerOptions().position(cubebustersREye));
+        LatLng cubebustersLEye = new LatLng(-75, 92);
+        mMap.addMarker(new MarkerOptions().position(cubebustersLEye));
+
+        LatLng cubebustersSmile1 = new LatLng(-78.2, 62);
+        mMap.addMarker(new MarkerOptions().position(cubebustersSmile1));
+        LatLng cubebustersSmile2 = new LatLng(-79.4, 67);
+        mMap.addMarker(new MarkerOptions().position(cubebustersSmile2));
+        LatLng cubebustersSmile3 = new LatLng(-80.6, 72);
+        mMap.addMarker(new MarkerOptions().position(cubebustersSmile3));
+        LatLng cubebustersSmile4 = new LatLng(-81, 77);
+        mMap.addMarker(new MarkerOptions().position(cubebustersSmile4));
+        LatLng cubebustersSmile5 = new LatLng(-81, 82);
+        mMap.addMarker(new MarkerOptions().position(cubebustersSmile5));
+        LatLng cubebustersSmile6 = new LatLng(-81, 87);
+        mMap.addMarker(new MarkerOptions().position(cubebustersSmile6));
+        LatLng cubebustersSmile7 = new LatLng(-80.6, 92);
+        mMap.addMarker(new MarkerOptions().position(cubebustersSmile7));
+        LatLng cubebustersSmile8 = new LatLng(-79.4, 97);
+        mMap.addMarker(new MarkerOptions().position(cubebustersSmile8));
+        LatLng cubebustersSmile9 = new LatLng(-78.2, 102);
+        mMap.addMarker(new MarkerOptions().position(cubebustersSmile9));
+
+
+
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(cubebustersHq));
+    }
+
 
     private void homeClick(View v){
         Intent intent = new Intent(this, MainActivity.class);
