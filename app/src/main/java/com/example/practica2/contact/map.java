4d8e23a8a6d6e4bb61a18b004f84a433a64a6751
JavsJavs <45043131+JavsJavs@users.noreply.github.com>
2021-06-03 02:22:55 +0200

    Map code cleanup

--- app/src/main/java/com/example/practica2/contact/map.java
+++ app/src/main/java/com/example/practica2/contact/map.java
@@ -43,42 +43,31 @@ public class map extends FragmentActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         // Add a marker in Sydney and move the camera
-        LatLng cubebustersHq = new LatLng(40.538430, -3.893596);
-        mMap.addMarker(new MarkerOptions().position(cubebustersHq).title("Cubebusters Headquarters"));
-        LatLng cubebustersMobile = new LatLng(20, -40);
-        mMap.addMarker(new MarkerOptions().position(cubebustersMobile).title("Cubebusters Mobile"));
-        LatLng cubebustersAsia = new LatLng(69, 69);
-        mMap.addMarker(new MarkerOptions().position(cubebustersAsia).title("Cubebusters Asia"));
+        LatLng cubeBustersHq = new LatLng(40.538430, -3.893596);
+        mMap.addMarker(new MarkerOptions().position(cubeBustersHq).title("Cubebusters Headquarters"));
+        LatLng cubeBustersMobile = new LatLng(20, -40);
+        mMap.addMarker(new MarkerOptions().position(cubeBustersMobile).title("Cubebusters Mobile"));
+        LatLng cubeBustersAsia = new LatLng(69, 69);
+        mMap.addMarker(new MarkerOptions().position(cubeBustersAsia).title("Cubebusters Asia"));
 
+        LatLng[] cubeBustersSmile = new LatLng[11];
+        cubeBustersSmile[0] = new LatLng(-78.2, 62);
+        cubeBustersSmile[1] = new LatLng(-79.4, 67);
+        cubeBustersSmile[2] = new LatLng(-80.6, 72);
+        cubeBustersSmile[3] = new LatLng(-81, 77);
+        cubeBustersSmile[4] = new LatLng(-81, 82);
+        cubeBustersSmile[5] = new LatLng(-81, 87);
+        cubeBustersSmile[6] = new LatLng(-80.6, 92);
+        cubeBustersSmile[7] = new LatLng(-79.4, 97);
+        cubeBustersSmile[8] = new LatLng(-78.2, 102);
+        cubeBustersSmile[9] = new LatLng(-75, 72);
+        cubeBustersSmile[10] = new LatLng(-75, 92);
 
-        LatLng cubebustersREye = new LatLng(-75, 72);
-        mMap.addMarker(new MarkerOptions().position(cubebustersREye));
-        LatLng cubebustersLEye = new LatLng(-75, 92);
-        mMap.addMarker(new MarkerOptions().position(cubebustersLEye));
+        for(int i = 0; i < 11; i++){
+            mMap.addMarker(new MarkerOptions().position(cubeBustersSmile[i]));
+        }
 
-        LatLng cubebustersSmile1 = new LatLng(-78.2, 62);
-        mMap.addMarker(new MarkerOptions().position(cubebustersSmile1));
-        LatLng cubebustersSmile2 = new LatLng(-79.4, 67);
-        mMap.addMarker(new MarkerOptions().position(cubebustersSmile2));
-        LatLng cubebustersSmile3 = new LatLng(-80.6, 72);
-        mMap.addMarker(new MarkerOptions().position(cubebustersSmile3));
-        LatLng cubebustersSmile4 = new LatLng(-81, 77);
-        mMap.addMarker(new MarkerOptions().position(cubebustersSmile4));
-        LatLng cubebustersSmile5 = new LatLng(-81, 82);
-        mMap.addMarker(new MarkerOptions().position(cubebustersSmile5));
-        LatLng cubebustersSmile6 = new LatLng(-81, 87);
-        mMap.addMarker(new MarkerOptions().position(cubebustersSmile6));
-        LatLng cubebustersSmile7 = new LatLng(-80.6, 92);
-        mMap.addMarker(new MarkerOptions().position(cubebustersSmile7));
-        LatLng cubebustersSmile8 = new LatLng(-79.4, 97);
-        mMap.addMarker(new MarkerOptions().position(cubebustersSmile8));
-        LatLng cubebustersSmile9 = new LatLng(-78.2, 102);
-        mMap.addMarker(new MarkerOptions().position(cubebustersSmile9));
-
-
-
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(cubebustersHq));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(cubeBustersHq));
     }
 
 
